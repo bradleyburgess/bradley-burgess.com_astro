48c892bb4a9cb6e18bed973bc74ec7f59f46718b
Bradley Burgess <bradleyburgess@gmail.com>
2024-05-20 22:29:36 -0400

    Make YouTubeEmbed component use inView

--- src/components/YouTubeEmbed.tsx
+++ src/components/YouTubeEmbed.tsx
@@ -1,19 +1,34 @@
+import { useEffect, useRef, useState } from "preact/hooks";
+import { useInView } from "react-intersection-observer";
+import { Suspense, lazy } from "preact/compat";
+// const Plyr = lazy(() => import("plyr-react"));
+
 type Props = {
   src: string;
 };
-const YouTubeEmbed = ({ src }: Props) => (
-  <div className="full-bleed aspect-video">
-    <iframe
-      className="h-full w-full"
-      width="100%"
-      src={src}
-      title="YouTube video player"
-      frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      referrerpolicy="strict-origin-when-cross-origin"
-      allowFullScreen
-    ></iframe>
-  </div>
-);
+const YouTubeEmbed = ({ src }: Props) => {
+  const { inView, ref } = useInView({
+    triggerOnce: true,
+  });
+
+  return (
+    <Suspense fallback={() => <div>Loading player ...</div>}>
+      <div className="full-bleed aspect-video" ref={ref}>
+        {inView && (
+          <iframe
+            className="h-full w-full"
+            width="100%"
+            src={src}
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+        )}
+      </div>
+    </Suspense>
+  );
+};
 
 export default YouTubeEmbed;
